e291fec8a51f62c4a06c9a9b1802f035693a785b
Jimmy Israkhan <jimmy.israkhan@gmail.com>
2021-09-28 15:57:33 +0700

    Changed font on Hero component.

--- src/components/_pages/home/hero.tsx
+++ src/components/_pages/home/hero.tsx
@@ -4,9 +4,24 @@ export function HeroBannerComponent() {
     <section className='ui--home-hero'>
       <div className='ui--home-hero-content'>
         <img className='coin' src='/static/images/003.png' alt='wwin-coin' />
-        <h2 className='h2'>Welcome to the</h2>
-        <h1 className='h1'>First SNFT Marketplace,</h1>
-        <h4 className='h4'>
+        <h2 style={{
+        fontFamily: 'made-bon-voyage-regular',
+        fontWeight: 900,
+        fontSize: '44px',
+        color: '#a58231'
+      }}>Welcome to the</h2>
+        <h1 style={{
+        fontFamily: 'made-bon-voyage-regular',
+        fontWeight: 900,
+        fontSize: '49px',
+        color: 'white'
+      }}>First SNFT Marketplace,</h1>
+        <h4 style={{
+        fontFamily: 'made-bon-voyage-regular',
+        fontWeight: 900,
+        fontSize: '18px',
+        color: '#abacba'
+      }}>
           where blockchain technology
           <br />
           meets ancient Tradition.
