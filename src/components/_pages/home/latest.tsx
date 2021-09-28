d8e528d70ba36fed24aa3c0c96de3a47ea936d42
Jimmy Israkhan <jimmy.israkhan@gmail.com>
2021-09-28 15:45:20 +0700

    Added new items and fix router to home page.

--- src/components/_pages/home/latest.tsx
+++ src/components/_pages/home/latest.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { MediaComponent } from '@/components'
+import { RouterLink, MediaComponent } from '@/components'
 
 export function LatestAssetsComponent() {
   // __STATE <React.Hooks>
@@ -8,15 +8,30 @@ export function LatestAssetsComponent() {
       id: 1,
       name: 'Gold Model',
       media: '/static/media/Garuda_Gold_Model.jpg',
-      link: 'https://wwin-reserve.web.app',
-      btnLabel: 'Purchase'
+      link: '/assets/0x8351057B11d48a02D138637C9386a7bCE72b0966',
+      btnLabel: 'Buy Now',
     },
     {
       id: 2,
       name: 'White Gold Model',
       media: '/static/media/Garuda_Silver_Model.jpeg',
-      link: 'https://stake.winwinwintoken.com/garuda-white-gold',
-      btnLabel: 'Stake'
+      link: '/assets/0xd1a21d267c5ae768ef9f75f38b16e03490c49e4e',
+      btnLabel: 'Buy Now',
+      external: false
+    },
+    {
+      id: 3,
+      name: 'Rose Gold Model',
+      media: '/static/media/nft/garuda_v02-rose_gold-thumb_m.jpg',
+      link: '#',
+      btnLabel: 'Coming Soon',
+    },
+    {
+      id: 4,
+      name: 'Graphite Model',
+      media: '/static/media/nft/garuda_v02-graphite-thumb_m.jpg',
+      link: '#',
+      btnLabel: 'Coming Soon',
     }
   ])
 
@@ -37,16 +52,16 @@ export function LatestAssetsComponent() {
             </div>
 
             <div className='ui--article-footer'>
-              <a className='meta' href={record.link}>
+              <RouterLink className='meta' href={record.link} useLink={record.external}>
                 <div className='meta-rows'>
                   <div className='name'>{record.name}</div>
                 </div>
-              </a>
+              </RouterLink>
 
               <div className='annotations'>
-                <a className='btn btn-primary' href={record.link}>
+                <RouterLink className='btn btn-primary' href={record.link} useLink={record.external}>
                   <span className='text'>{record.btnLabel}</span>
-                </a>
+                </RouterLink>
               </div>
             </div>
           </article>
