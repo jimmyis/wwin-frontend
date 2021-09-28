f7baf32ae815164bccc9df98e581e97b5682bcd9
Jimmy Israkhan <jimmy.israkhan@gmail.com>
2021-09-28 15:50:29 +0700

    Temp removed misbehave MetaMask connector.

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -1,6 +1,6 @@
-import { configs, chian } from '@/libs/configs'
-import { getRpcUrl } from '@/libs/web3'
-import { capitalize } from '@/utils'
+import { configs, /* chain */ } from '@/libs/configs'
+// import { getRpcUrl } from '@/libs/web3'
+// import { capitalize } from '@/utils'
 
 /**
  * Prompt the user to add BSC as a network on Metamask, or switch to BSC if the wallet is on a different network
@@ -11,23 +11,23 @@ export async function setNetwork(cb: (res: boolean) => void): Promise<void> {
 
   if (ethereum?.request) {
     try {
-      await ethereum.request({
-        method: 'wallet_addEthereumChain',
-        params: [
-          {
-            chainId: `0x${chian.chianId.toString(16)}`,
-            chainName: `Binance Smart Chain ${capitalize(chian.network)}`,
-            nativeCurrency: {
-              name: 'BNB',
-              symbol: 'bnb',
-              decimals: 18
-            },
-            rpcUrls: [getRpcUrl()],
-            blockExplorerUrls: [chian.explorer]
-          }
-        ]
-      })
-      cb(true)
+      // await ethereum.request({
+      //   method: 'wallet_addEthereumChain',
+      //   params: [
+      //     {
+      //       chainId: `0x${chain.chainId.toString(16)}`,
+      //       chainName: `Binance Smart Chain ${capitalize(chain.network)}`,
+      //       nativeCurrency: {
+      //         name: 'BNB',
+      //         symbol: 'bnb',
+      //         decimals: 18
+      //       },
+      //       rpcUrls: [getRpcUrl()],
+      //       blockExplorerUrls: [chain.explorer]
+      //     }
+      //   ]
+      // })
+      // cb(true)
     } catch (error) {
       cb(false)
       console.error('Failed to setup the network in Metamask:', error)
