14ce144d5a884010fae286e90e0e939718fa3e10
Jimmy Israkhan <jimmy.israkhan@gmail.com>
2021-09-29 17:48:28 +0700

    Fixed data getting logic on profile page.
    - Solve the case that address string mismatch by letter case.

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react'
 import { Profile, Loading, ArticleComponent } from '@/components'
-import { userService } from '@/services/user.service'
+// import { userService } from '@/services/user.service'
 import { useSelector, authSelector } from '@/store'
 import { NFTItem } from '@/types'
 import { db } from '@/libs/firebase'
-import { doc, getDoc, query, where, collection, getDocs } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 
 export default function ProfileContainer() {
   // __STATE <React.Hooks>
@@ -27,40 +27,43 @@ export default function ProfileContainer() {
 
       const records_: any[] = []
       
-      const docUserOwnerRef = doc(db, "user_owned_token", user?.uid || "")
-      const docUserOwnerSnap = await getDoc(docUserOwnerRef)
-      const userOwnerData = docUserOwnerSnap.data() as any
-
-      const userOwnerList = Object.keys(userOwnerData)
-
-      for (let erc721 of userOwnerList) {
-
-        const docCollectionsRef = doc(db, "nft_collections", erc721)
-        const docCollectionsSnap = await getDoc(docCollectionsRef)
-
-        if (docCollectionsSnap.exists()) {
-          const collectionsData = docCollectionsSnap.data() as any
-
-          // const docTokenOwnedRef = doc(db, "nft_tokens_list:owned", erc721)
-          // const docTokenOwnedSnap = await getDoc(docTokenOwnedRef)
-
-          // let tokenOwnedData = {}
-
-          // if (docTokenOwnedSnap.exists()) {
-          //   tokenOwnedData = docTokenOwnedSnap.data() as any
-          // }
-
-          if (Object.keys(collectionsData).length > 0) {
-            const data = {...collectionsData, serialNoList: userOwnerData[erc721].serial_no}
-            records_?.push(data)
+      const wallet_address = (user?.uid as string).toLowerCase() || null
+      if (wallet_address) {
+        const docUserOwnerRef = doc(db, "user_owned_token", wallet_address)
+        const docUserOwnerSnap = await getDoc(docUserOwnerRef)
+        const userOwnedData = docUserOwnerSnap.data() as any
+  
+        const userOwnedList = Object.keys(userOwnedData)
+  
+        for (let erc721 of userOwnedList) {
+          const erc721_address = erc721.toLowerCase();
+          const docCollectionsRef = doc(db, "nft_collections", erc721_address)
+          const docCollectionsSnap = await getDoc(docCollectionsRef)
+  
+          if (docCollectionsSnap.exists()) {
+            const collectionsData = docCollectionsSnap.data() as any
+  
+            // const docTokenOwnedRef = doc(db, "nft_tokens_list:owned", erc721)
+            // const docTokenOwnedSnap = await getDoc(docTokenOwnedRef)
+  
+            // let tokenOwnedData = {}
+  
+            // if (docTokenOwnedSnap.exists()) {
+            //   tokenOwnedData = docTokenOwnedSnap.data() as any
+            // }
+  
+            if (Object.keys(collectionsData).length > 0) {
+              const data = {...collectionsData, serialNoList: userOwnedData[erc721].serial_no}
+              records_?.push(data)
+            }
           }
+          
+          console.log('in map object end', erc721)
         }
-        
-        console.log('in map object end', erc721)
+  
+        console.log('record', records_)
+        setState(records_)
       }
-
-      console.log('record', records_)
-      setState(records_)
     }
     if (user) run()
   }, [user])
